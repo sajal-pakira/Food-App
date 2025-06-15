4287969b24baa458e8425efbea07c3b792830143
Sajal Pakira <uniquesajal6@gmail.com>
2025-06-15 18:45:01 +0530

    created dummy data for creating a restaurant

---
+++ data/dummyRestaurantData.js
@@ -0,0 +1,31 @@
+const data = {
+  "title": "SpiceHub Restaurant",
+  "imageUrl": "https://example.com/images/spicehub-banner.jpg",
+  "foods": [
+    {
+      "name": "Butter Chicken",
+      "price": 320,
+      "category": "Non-Veg",
+      "image": "https://example.com/images/butter-chicken.jpg"
+    },
+    {
+      "name": "Paneer Tikka",
+      "price": 250,
+      "category": "Veg",
+      "image": "https://example.com/images/paneer-tikka.jpg"
+    }
+  ],
+  "time": "10:00 AM - 11:00 PM",
+  "pickup": true,
+  "delivery": true,
+  "isopen": true,
+  "logoUrl": "https://example.com/images/spicehub-logo.png",
+  "rating": 4.3,
+  "ratingCount": 126,
+  "code": "SPC123",
+  "address": "23 Park Street, Kolkata, West Bengal 700016",
+  "coords": {
+    "lat": 22.5565,
+    "lng": 88.3520
+  }
+}
